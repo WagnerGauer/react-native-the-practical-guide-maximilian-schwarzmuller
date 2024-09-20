2429705f39021ce32f353c78de9968396efdf792
Wagner Gauer <wggauer@gmail.com>
2024-09-19 23:30:58 -0300

    Create screens and button component, add text input and buttons to start game screen & coarsely create primary button component

---
+++ AwesomeProject/screens/StartGameScreen.js
@@ -0,0 +1,47 @@
+import { StyleSheet, Text, TextInput, View } from "react-native";
+import React from "react";
+import PrimaryButton from "../components/PrimaryButton";
+
+const StartGameScreen = () => {
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
+    </View>
+  );
+};
+
+export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    padding: 16,
+    marginHorizontal: 24,
+    borderRadius: 8,
+    marginTop: 100,
+    backgroundColor: "#6b2b2b",
+    elevation: 8,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: "#af6709",
+    borderBottomWidth: 2,
+    color: "#af6709",
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
