30e7bdc7a449e87748b7e5cd8b30de8e62603d69
Wagner Gauer <wggauer@gmail.com>
2024-09-25 09:40:09 -0300

    Dynamically set the appropriate title in the meal overview page

--- meals/screens/MealsOverviewScreen.js
+++ meals/screens/MealsOverviewScreen.js
@@ -1,15 +1,27 @@
+import { useLayoutEffect } from "react";
 import { FlatList, StyleSheet, Text, View } from "react-native";
 import React from "react";
-import { MEALS } from "../data/dummy-data";
+import { MEALS, CATEGORIES } from "../data/dummy-data";
 
 import MealItem from "../components/MealItem";
 
-const MealsOverviewScreen = ({ route }) => {
+const MealsOverviewScreen = ({ route, navigation }) => {
   const catId = route.params.categoryId;
 
   const displayedMeals = MEALS.filter((mealItem) =>
     mealItem.categoryIds.includes(catId)
   );
+
+  const categoryTitle = CATEGORIES.find(
+    (category) => category.id === catId
+  ).title;
+  console.log(categoryTitle);
+
+  // useLayoutEffet is the same as useEffect but it garanties that in this case the title
+  // doesn't load after the component is rendered
+  useLayoutEffect(() => {
+    navigation.setOptions({ title: categoryTitle });
+  }, [catId, navigation]);
 
   function renderMealItem({ item }) {
     const { title, imageUrl, affordability, complexity, duration } = item;
