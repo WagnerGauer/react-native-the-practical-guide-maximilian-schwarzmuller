6c3ad33f874f7b432d94719ac79234a4d855d094
Wagner Gauer <wggauer@gmail.com>
2024-09-25 22:32:55 -0300

    Add drawer navigation

--- differentNavigations/App.js
+++ differentNavigations/App.js
@@ -1,20 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import WelcomeScreen from "./screens/WelcomeScreen";
+import UserScreen from "./screens/UserScreen";
+import { Ionicons } from "@expo/vector-icons";
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Drawer.Navigator
+        screenOptions={{
+          drawerActiveBackgroundColor: "#b8b7f1",
+          drawerActiveTintColor: "#3c3b86",
+          // drawerStyle: { backgroundColor: "#dbdbdb" },
+          headerStyle: { backgroundColor: "#3c3b86" },
+          headerTintColor: "white",
+        }}
+      >
+        <Drawer.Screen
+          name="Wellcome"
+          component={WelcomeScreen}
+          options={{
+            drawerLabel: "Wecome Screen",
+            drawerIcon: ({ color }) => (
+              <Ionicons name="home" color={color} size={18} />
+            ),
+          }}
+        />
+        <Drawer.Screen
+          name="User"
+          component={UserScreen}
+          options={{
+            drawerIcon: ({ color, size }) => (
+              <Ionicons name="person" color={color} size={size} />
+            ),
+          }}
+        />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
